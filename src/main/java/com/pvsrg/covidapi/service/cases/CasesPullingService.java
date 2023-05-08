544ff5463f13fd36a6ddae8715798c6eb8235a7e
sergeypavlihin <sergey.pavlikhin44@gmail.com>
2023-05-08 20:11:19 +0800

    Lock

--- src/main/java/com/pvsrg/covidapi/service/cases/CasesPullingService.java
+++ src/main/java/com/pvsrg/covidapi/service/cases/CasesPullingService.java
@@ -10,46 +10,59 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class CasesPullingService {
+
+    private final ReentrantLock lock = new ReentrantLock();
+
     private final CovidApiService covidApiService;
     private final CasesService casesService;
     private final CountryService countryService;
 
     public void pullCases() {
-        log.info("Begin pulling cases...");
-        for (CountryEntity country : countryService.findAll()) {
+        if (lock.tryLock()) {
             try {
-                log.info("Try to pull cases for {}", country.getName());
-                var casesData = covidApiService.fetchCases(country.getSlug())
-                        .stream()
-                        .sorted(Comparator.comparing(CACasesDataDTO::date))
-                        .toList();
+                log.info("Begin pulling cases...");
+                for (CountryEntity country : countryService.findAll()) {
+                    try {
+                        log.info("Try to pull cases for {}", country.getName());
+                        var casesData = covidApiService.fetchCases(country.getSlug())
+                                .stream()
+                                .sorted(Comparator.comparing(CACasesDataDTO::date))
+                                .toList();
 
-                for (int i = 0; i < casesData.size(); i++) {
+                        for (int i = 0; i < casesData.size(); i++) {
 
-                    var it = casesData.get(i);
+                            var it = casesData.get(i);
 
-                    // API doesn't provide exactly number of new cases, but provides total amount of cases per day.
-                    // So we can calculate new cases like: current day cases - previous day cases.
-                    // Don't forget that first element in list - first day of COVID pandemic
-                    var newCases = i == 0 ? it.cases() : it.cases() - casesData.get(i - 1).cases();
+                            // API doesn't provide exactly number of new cases, but provides total amount of cases per day.
+                            // So we can calculate new cases like: current day cases - previous day cases.
+                            // Don't forget that first element in list - first day of COVID pandemic
+                            var newCases = i == 0 ? it.cases() : it.cases() - casesData.get(i - 1).cases();
 
-                    casesService.insert(country.getId(), it.date(), Math.max(0, newCases));
+                            casesService.insert(country.getId(), it.date(), Math.max(0, newCases));
+                        }
+
+                        log.info("Finished pulling cases for {}", country.getName());
+                    } catch (CAException e) {
+                        log.error("Couldn't fetch cases from external source", e);
+                    } catch (Exception e) {
+                        log.error("Couldn't pull cases", e);
+                    }
                 }
 
-                log.info("Finished pulling cases for {}", country.getName());
-            } catch (CAException e) {
-                log.error("Couldn't fetch cases from external source", e);
-            } catch (Exception e) {
-                log.error("Couldn't pull cases", e);
+                log.info("Finished pulling cases...");
+            } finally {
+                lock.unlock();
             }
+        } else {
+            log.info("Pulling in progress, try again later");
         }
 
-        log.info("Finished pulling cases...");
     }
 
 }
--- src/main/java/com/pvsrg/covidapi/service/country/CountryPullingService.java
+++ src/main/java/com/pvsrg/covidapi/service/country/CountryPullingService.java
@@ -6,22 +6,33 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CountryPullingService {
 
+    private final ReentrantLock lock = new ReentrantLock();
+
     private final CovidApiService covidApiService;
     private final CountryService countryService;
 
     public void pullCountries() {
-        try {
-            var countriesResponseData = covidApiService.fetchCountries();
-            countriesResponseData.forEach(it -> countryService.saveCountry(it.name(), it.slug()));
-        } catch (CAException e) {
-            log.error("Couldn't fetch countries from external source", e);
-        } catch (Exception e) {
-            log.error("Couldn't pull countries", e);
+        if (lock.tryLock()) {
+            try {
+                var countriesResponseData = covidApiService.fetchCountries();
+                countriesResponseData.forEach(it -> countryService.saveCountry(it.name(), it.slug()));
+            } catch (CAException e) {
+                log.error("Couldn't fetch countries from external source", e);
+            } catch (Exception e) {
+                log.error("Couldn't pull countries", e);
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            log.info("Pulling in progress, try again later");
         }
+
     }
 }
