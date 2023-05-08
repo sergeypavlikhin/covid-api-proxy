a6f1a40d92eef833fa24826a43667df661929eb2
sergeypavlihin <sergey.pavlikhin44@gmail.com>
2023-05-08 20:34:28 +0800

    Cleanup

--- src/main/java/com/pvsrg/covidapi/properties/AppProperties.java
+++ src/main/java/com/pvsrg/covidapi/properties/AppProperties.java
@@ -4,9 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "app")
 public record AppProperties(
-
         String externalSourceURL,
-
         long requestDelayMs,
         long coolingTimeSec,
         double coolingTimeMultiplier,
